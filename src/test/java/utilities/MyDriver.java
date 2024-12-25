772055733100e3fc9b98b01a830bca190b0952f3
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-25 13:19:50 +0300

    Update MyDriver.java

--- src/test/java/utilities/MyDriver.java
+++ src/test/java/utilities/MyDriver.java
@@ -1,4 +1,27 @@
 package utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 
 public class MyDriver {
+    private static WebDriver driver;
+
+
+    public static WebDriver getDriver(){
+        if (driver==null){
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }
+        return  driver;
+    }
+    public static void closeDriver(){
+        if (driver!=null){
+            driver.quit();
+            driver=null;
+        }
+    }
+
 }
